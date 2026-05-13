6151aaee2375dcb43838c10de71c16dae840e70d
Rabindra Kar <rabindra.kar@oohas.com>
2026-05-13 17:13:52 +0530

    model changes

--- main.py
+++ main.py
@@ -29,13 +29,61 @@
 
 
 
+# from fastapi import FastAPI, UploadFile, File
+# from faster_whisper import WhisperModel
+# from deep_translator import GoogleTranslator
+
+# app = FastAPI()
+
+# model = WhisperModel("base")
+
+# @app.get("/")
+# def home():
+#     return {
+#         "message": "Backend Working"
+#     }
+
+# @app.post("/translate_voice")
+# async def translate_voice(
+#     audio: UploadFile = File(...)
+# ):
+
+#     audio_path = f"temp_{audio.filename}"
+
+#     with open(audio_path, "wb") as f:
+#         f.write(await audio.read())
+
+#     segments, info = model.transcribe(audio_path)
+
+#     original_text = ""
+
+#     for segment in segments:
+#         original_text += segment.text
+
+#     translated = GoogleTranslator(
+#         source='auto',
+#         target='en'
+#     ).translate(original_text)
+
+#     return {
+#         "language": info.language,
+#         "original_text": original_text,
+#         "translated_text": translated
+#     }
+
 from fastapi import FastAPI, UploadFile, File
 from faster_whisper import WhisperModel
 from deep_translator import GoogleTranslator
+import os
 
 app = FastAPI()
 
-model = WhisperModel("base")
+# Better model for Indian languages
+model = WhisperModel(
+    "medium",
+    device="cpu",
+    compute_type="int8"
+)
 
 @app.get("/")
 def home():
@@ -43,30 +91,39 @@ def home():
         "message": "Backend Working"
     }
 
-@app.post("/translate_voice")
+@app.post("/translate")
 async def translate_voice(
     audio: UploadFile = File(...)
-):
+    ):
 
     audio_path = f"temp_{audio.filename}"
 
     with open(audio_path, "wb") as f:
         f.write(await audio.read())
 
-    segments, info = model.transcribe(audio_path)
+    # AUTO DETECT LANGUAGE
+    segments, info = model.transcribe(
+        audio_path,
+        beam_size=5
+    )
 
     original_text = ""
 
     for segment in segments:
-        original_text += segment.text
+        original_text += segment.text + " "
 
+    # TRANSLATE TO ENGLISH
     translated = GoogleTranslator(
         source='auto',
         target='en'
     ).translate(original_text)
 
+    # DELETE TEMP FILE
+    os.remove(audio_path)
+
     return {
-        "language": info.language,
-        "original_text": original_text,
+        "detected_language": info.language,
+        "language_probability": info.language_probability,
+        "original_text": original_text.strip(),
         "translated_text": translated
     }
