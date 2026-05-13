204148c230681b1330992d7c9fe975884454baea
Rabindra Kar <rabindra.kar@oohas.com>
2026-05-13 16:13:37 +0530

    new model added

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# from deep_translator import GoogleTranslator
+
+# app = FastAPI()
+
+# @app.get("/")
+# def home():
+#     return {
+#         "message": "Backend Working"
+#     }
+
+# class TranslateRequest(BaseModel):
+#     text: str
+#     source_lang: str
+#     target_lang: str
+
+# @app.post("/translate")
+# async def translate(req: TranslateRequest):
+
+#     translated = GoogleTranslator(
+#         source=req.source_lang,
+#         target=req.target_lang
+#     ).translate(req.text)
+
+#     return {
+#         "translated_text": translated
+#     }
+
+
+
+from fastapi import FastAPI, UploadFile, File
+from faster_whisper import WhisperModel
 from deep_translator import GoogleTranslator
 
 app = FastAPI()
+
+model = WhisperModel("base")
 
 @app.get("/")
 def home():
@@ -10,19 +43,30 @@ def home():
         "message": "Backend Working"
     }
 
-class TranslateRequest(BaseModel):
-    text: str
-    source_lang: str
-    target_lang: str
+@app.post("/translate_voice")
+async def translate_voice(
+    audio: UploadFile = File(...)
+):
 
-@app.post("/translate")
-async def translate(req: TranslateRequest):
+    audio_path = f"temp_{audio.filename}"
+
+    with open(audio_path, "wb") as f:
+        f.write(await audio.read())
+
+    segments, info = model.transcribe(audio_path)
+
+    original_text = ""
+
+    for segment in segments:
+        original_text += segment.text
 
     translated = GoogleTranslator(
-        source=req.source_lang,
-        target=req.target_lang
-    ).translate(req.text)
+        source='auto',
+        target='en'
+    ).translate(original_text)
 
     return {
+        "language": info.language,
+        "original_text": original_text,
         "translated_text": translated
     }
